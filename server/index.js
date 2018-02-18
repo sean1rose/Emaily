dacf21ecf7ece0c6bb0063ab8e01ab79ada26b92
Sean Rose <sean1rose@gmail.com>
2018-02-17 23:24:20 -0800

    set up google oauth keys

--- server/index.js
+++ server/index.js
@@ -1,17 +1,19 @@
 // COMMONJS modules on SERVER side (using require; as opposed to es2015 modules which use 'import')
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys.js');
+
 const app = express();
 
-// route handler
-app.get('/', (req, res) => {
-  // send response
-  res.send({ peace: 'outttttt!'});
-});
+
+// use passport to use new instance of google 2.0 strategy
+  // need to pass client id and client secret to GoogleStrategy
+passport.use(new GoogleStrategy());
 
 
 // heroku injects environment variables (set in node runtime)
-  // if there's an env variable defined by heroku, assign it to port
-  // otherwise you're developing so use 5000
+  // if there's an env variable defined by heroku, assign it to PORT (otherwise you're developing so use 5000)
 const PORT = process.env.PORT || 5000;
 // express telling node to listen to port 5000
 app.listen(PORT);
